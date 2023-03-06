e8a895f3212cb10f01c3b0052cf4fcdd05c3b1b3
Michal Rella <rel39331@spseol.cz>
2023-03-06 13:54:45 +0100

    ve skole

--- grafy_RELLA.py
+++ grafy_RELLA.py
@@ -1,3 +1,4 @@
+"""
 import pylab as pl
 from pylab import pi
 
@@ -18,3 +19,55 @@ pl.legend()
 
 
 pl.show()
+
+"""
+
+"""
+from tkinter import messagebox
+from tkinter import filedialog
+import tkinter as tk
+
+
+
+
+
+def __init__(self, master-None,cnf-{}, **kw):
+    super().__init__(master, cnf, **kw)
+
+    if not"textvariable" in kw:
+        self.variable = tk.StringVar()
+        self.config(textvariable=self.variable)
+    else:
+        self.variable = kw["textvariable"]
+
+@property
+def value():
+    return self.variable.get()
+
+@value.setter
+def value(self, new: str):
+    self.variable.set(new)
+
+"""
+
+import scipy.interpolate as inp
+import pylab as lab
+
+#x= [0, 0.3, 0.5, 0.8, 1,  2,  3 ]
+#y= [0, 0.1, 0.5, 1,   3, 10, 30]
+
+x= "0 0.3 0.5 0.8 1  2  3".split()
+y= "0 0.1 0.5 1   3 10 30".split()
+
+x = list(map(float, x))
+y = list(map(float, y))
+
+spl = inp.CubicSpline(x,y)
+newX = lab.linspace (0,3,2000)
+newY = spl(newX)
+lab.plot(newX,newY,"-")
+lab.plot(x,y,"o",color = "red")
+lab.show()
+
+
+
